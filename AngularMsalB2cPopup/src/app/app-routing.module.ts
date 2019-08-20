545483fd37be15fa64a5cf954b931863f68c976c
David Laptop <foeman@hotmail.com>
2019-08-19 22:24:24 -0500

    fix issues

--- AngularMsalB2cPopup/src/app/app-routing.module.ts
+++ AngularMsalB2cPopup/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { AuthGuard } from './core/guards/auth.guard';
+import { SimpsonsComponent } from './components/simpsons/simpsons.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'welcome', component: WelcomeComponent, canActivate: [AuthGuard] },
+  { path: 'simpsons', component: SimpsonsComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: '**', component: WelcomeComponent }
 ];
--- AngularMsalB2cPopup/src/app/app.module.ts
+++ AngularMsalB2cPopup/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/AppComponent/app.component';
+import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { SimpsonsComponent } from './components/simpsons/simpsons.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    SimpsonsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
